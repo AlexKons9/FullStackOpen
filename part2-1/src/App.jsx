8a6d0e3e18dfae0537c8b3cab17ce3fc979c72c5
AlexKons9 <alex.lepeniotis@gmail.com>
2024-01-18 16:34:35 +0200

    Finished 2e part  of presentation

--- part2-1/src/App.jsx
+++ part2-1/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState, useEffect } from "react";
 import noteService from "./services/notes";
 import Note from "./components/Note";
+import Notification from "./components/Notification"
+import Footer from "./components/Footer"
+import './index.css'
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState()
+  
 
   const toggleImportanceOf = (id) => {
     const note = notes.find((n) => n.id === id);
@@ -17,7 +22,12 @@ const App = () => {
         setNotes(notes.map(note => (note.id !== id ? note : returnedNote)));
       })
       .catch((error) => {
-        alert(`the note '${note.content}' was already deleted from server`);
+        setErrorMessage(
+          `Error for note '${note.content}'.`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter((n) => n.id !== id));
       });
   };
@@ -52,6 +62,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      {errorMessage ? <Notification message={errorMessage} /> : ''}
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
@@ -73,6 +84,7 @@ const App = () => {
         />
         <button type="submit">save</button>
       </form>
+      <Footer/>
     </div>
   );
 };
